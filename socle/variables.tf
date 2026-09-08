b364bb7e565c2d0e2904d4a588d7767299fda00e
aichaaa05 <aissatou.camara@edu.esiee.fr>
2026-09-08 14:03:33 +0000

    Lot 1 Etape 2

---
+++ socle/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "Identifiant du projet Google Cloud"
+  type        = string
+}
+
+variable "region" {
+  description = "Région Google Cloud utilisée par le projet"
+  type        = string
+}
